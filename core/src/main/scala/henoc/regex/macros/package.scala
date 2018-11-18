2c3413f336fb0e577404dfcdda1aa6c72e11ea92
Henoc <agekutar@gmail.com>
2018-11-18 15:48:18 +0900

    refactor

--- core/src/main/scala/henoc/regex/macros/package.scala
+++ core/src/main/scala/henoc/regex/macros/package.scala
@@ -1,55 +1,48 @@
 package henoc.regex
 
-import scala.language.dynamics
 import henoc.regex.refined.regex_string._
 import scala.reflect.macros.whitebox
 
 package object macros {
 
-  /**
-    * Provide a regex string interpolator for pattern matching.
-    * {{{
-    * "2018-11-18" match {
-    *   case regex"""(\d+$year)-(\d+$month)-(\d+$day)""" => println(s"year = $year, month = $month, day = $day")
-    *   case _ => println("no!")
-    * }
-    * }}}
-    *
-    * The variable positions of interpolator is not concerned, so you can also write:
-    * {{{
-    *   regex"""(\d+)-(\d+)-(\d+)$year$month$day"""
-    *   regex"""(?x)   (\d+)-(\d+)-(\d+)    # $year, $month, $day"""
-    * }}}
-    */
   implicit class RegexContext(sc: StringContext) {
-    def regex: RegexContextExtractor = macro provideExtractorInstance
+    /**
+      * Provide a regex string interpolator for pattern matching.
+      * {{{
+      *   "2018-11-18" match {
+      *     case regex"""(\d+$year)-(\d+$month)-(\d+$day)""" => println(s"year = $year, month = $month, day = $day")
+      *     case _ => println("no!")
+      *   }
+      * }}}
+      *
+      * The variable positions of interpolator is not concerned, so you can also write:
+      * {{{
+      *   regex"""(\d+)-(\d+)-(\d+)$year$month$day"""
+      *   regex"""(?x)   (\d+)-(\d+)-(\d+)    # $year, $month, $day"""
+      * }}}
+      */
+    def regex: RegexContextExtractor = new RegexContextExtractor(sc)
   }
 
-  def provideExtractorInstance(c: whitebox.Context): c.Tree = {
-    import c.universe._
-
-    c.prefix.tree match {
-      case Apply(_, List(Apply(_, rawParts))) =>
-        val parts = rawParts map { case Literal(Constant(const: String)) => const }
-        q"new RegexContextExtractor(${Literal(Constant(parts.mkString))})"
-      case _ =>
-        c.abort(c.enclosingPosition, "Invalid use of regex string extractor")
-    }
+  class RegexContextExtractor(sc: StringContext) {
+    def unapply(s: String): Any = macro MacroImpls.provideExtractorImitator
   }
 
-  def provideExtractorImitator(c: whitebox.Context)(s: c.Tree): c.Tree = {
-    import c.universe._
+  private[macros] object MacroImpls {
 
-    val regex = c.prefix.tree match {
-      case Apply(_, List(Literal(Constant(const: String)))) => const
-      case _ => c.abort(c.enclosingPosition, "Invalid use of regex string extractor")
-    }
-    val count = groupCount(compile(regex))
-    val imitationMethods = (1 to count)
-      .map(i => q"def ${TermName("_" + i)} = matched.get(${Literal(Constant(i - 1))})")
-      .toList
+    def provideExtractorImitator(c: whitebox.Context)(s: c.Tree): c.Tree = {
+      import c.universe._
 
-    q"""
+      val regex = c.prefix.tree match {
+        case Select(Apply(_, List(Apply(_, rawParts))), _) => rawParts.map{ case q"${const: String}" => const }.mkString
+        case _ => c.abort(c.enclosingPosition, s"Invalid use of regex string extractor")
+      }
+      val count = groupCount(compile(regex))
+      val imitationMethods = (1 to count)
+        .map(i => q"def ${TermName("_" + i)} = matched.get(${Literal(Constant(i - 1))})")
+        .toList
+
+      q"""
        new {
          var matched: Option[Array[String]] = None
          def isEmpty = matched.isEmpty
@@ -61,9 +54,6 @@ package object macros {
          }
        }.unapply($s)
      """
-  }
-
-  class RegexContextExtractor(regex: String) {
-    def unapply(s: String): Any = macro provideExtractorImitator
+    }
   }
 }
