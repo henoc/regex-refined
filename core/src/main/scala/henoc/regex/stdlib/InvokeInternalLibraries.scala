4e953845ad9f1cc4780e36b122ed6c0b11750ea5
Henoc <agekutar@gmail.com>
2018-11-23 19:36:09 +0900

    modified

--- core/src/main/scala/henoc/regex/stdlib/InvokeInternalLibraries.scala
+++ core/src/main/scala/henoc/regex/stdlib/InvokeInternalLibraries.scala
@@ -6,7 +6,7 @@ import sun.text.Normalizer
 /**
   * Invoke internal libraries from scala to suppress warnings.
   */
-private[stdlib] object InvokeInternals {
+private[stdlib] object InvokeInternalLibraries {
   def getCombiningClass(c: Int): Int = Normalizer.getCombiningClass(c)
   def newGetPropertyAction(s: String) = new GetPropertyAction(s)
 }
--- core/src/main/scala/henoc/regex/stdlib/PatternExtra.scala
+++ core/src/main/scala/henoc/regex/stdlib/PatternExtra.scala
@@ -1,13 +1,26 @@
 package henoc.regex.stdlib
 
 import java.util
+import java.util.regex.{PatternSyntaxException => PatternExceptionOriginal}
 
-private[regex] object Implicits {
+import scala.util.Try
 
-  implicit class PatternAccessor(val pattern: Pattern) extends AnyVal {
+private[regex] class PatternExtra(val pattern: Pattern) extends AnyVal {
 
-    def namedGroups(): util.Map[String, Integer] = pattern.namedGroups()
+  def namedGroups(): util.Map[String, Integer] = pattern.namedGroups()
 
-  }
+  def groupCount(): Int = pattern.matcher("").groupCount()
+
+  def flags(): Int = pattern.flags()
+
+  def matches(text: String): Boolean = pattern.matcher(text).matches()
+
+}
+private[regex] object PatternExtra {
+
+  def compile(regex: String): PatternExtra =
+    Try(new PatternExtra(Pattern.compile(regex))).recover {
+      case e: PatternSyntaxException => throw new PatternExceptionOriginal(e.getDescription, e.getPattern, e.getIndex)
+    }.get
 
 }
